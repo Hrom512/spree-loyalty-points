9eb2fd92fde970cb10efae9ae1f078487ec92ab5
hrom <roman@alltmb.ru>
2015-08-29 20:41:46 +0300

    html title for loyalty points page

--- app/controllers/spree/loyalty_points_controller.rb
+++ app/controllers/spree/loyalty_points_controller.rb
@@ -1,6 +1,7 @@
 class Spree::LoyaltyPointsController < Spree::StoreController
 
   def index
+    @title = Spree.t(:loyalty_points)
     @loyalty_points_transactions = spree_current_user.loyalty_points_transactions.includes(:source).order(updated_at: :desc).
       page(params[:page]).
       per(params[:per_page] || Spree::Config[:orders_per_page])
